57f63dafb968eb2d4f07cfae028876aab06f3f63
Todd Kronenberg <toddkronenberg@gmail.com>
2015-08-26 21:35:08 -0700

    Added createdAt field in user model

--- src/models/user.js
+++ src/models/user.js
@@ -7,7 +7,8 @@ const mongoose = require('mongoose'),
 
 const userSchema = new mongoose.Schema({
   email:        { type: String, required: true },
-  password:     { type: String, required: true }
+  password:     { type: String, required: true },
+  createdAt:    { type: Date, default: Date.now, required: true }
 });
 
 userSchema.statics.create = (user, cb) => {
